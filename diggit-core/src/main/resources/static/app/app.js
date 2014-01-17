a54d25713b340102193e2aea4359060147ec2844
Krzysztof Urman <krzysztof.urman@espeo.pl>
2014-01-17 11:30:55 +0100

    frontend: list link; enable simple voting.

--- diggit-core/src/main/resources/static/app/app.js
+++ diggit-core/src/main/resources/static/app/app.js
@@ -1,6 +1,27 @@
 var app = angular.module('diggit', []);
 
 app.controller('DiggitController', function($scope) {
-  $scope.date = new Date();
+  var sequencer = 100;
+
+  $scope.links = [];
+  $scope.newLink = '';
+
+
+  $scope.addLink = function() {
+    $scope.links.push({id: generateId(), path: $scope.newLink, votes: 0});
+    $scope.newLink = '';
+  };
+
+  $scope.vote = function(id, value) {
+    angular.forEach($scope.links, function(link){
+      if (link.id === id) {
+        link.votes = link.votes + value;
+      }
+    });
+  }
+
+  function generateId() {
+    return sequencer++;
+  }
 });
 
